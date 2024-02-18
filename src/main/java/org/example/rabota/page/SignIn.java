a4e82d923272688f1fdd1b42409737dd9a3c1f77
JimSega <dvigokv@gmail.com>
2024-02-18 22:46:29 +0300

    used @FindBy

--- src/main/java/org/example/rabota/page/SignIn.java
+++ src/main/java/org/example/rabota/page/SignIn.java
@@ -1,23 +1,49 @@
 package org.example.rabota.page;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class SignIn extends RabotaBasePage {
-    @FindBy(how = How.CLASS_NAME, using = "//div/button[contains(@class, 'user-profile-header__sign-in')]")
+    @FindBy(how = How.XPATH, using = "//div/button[contains(@class, 'user-profile-header__sign-in')]")
     private SelenideElement signIn;
+    //private SelenideElement signIn = $(By.xpath("//div/button[contains(@class, 'user-profile-header__sign-in')]"));
 
-    private final SelenideElement numberOrEmailInput = $("//input[@name='login']");
+    private final SelenideElement numberOrEmailInput = $(By.xpath("//input[@name='login']"));
     private final SelenideElement continuePressNumberOrEmail = $(".auth-main-step__button-content");
-    private final SelenideElement password = $("//input[@name='password']");
-    private final SelenideElement continuePassword = $("//button[contains(@class, 'auth-authorization-step__form-button')]");
+    private final SelenideElement passwordElement = $(By.xpath("//input[@name='password']"));
+    private final SelenideElement continuePassword = $(By.xpath("//button[contains(@class, 'auth-authorization-step__form-button')]"));
 
-    public RabotaBasePage open() {
+    public SignIn open() {
         super.open(getBaseUrl());
         return this;
     }
 
+    public SignIn clickEnter() {
+        signIn.click();
+        return this;
+    }
+
+    public SignIn setName(String name) {
+        numberOrEmailInput.val(name);
+        return this;
+    }
+
+    public SignIn clickSetName() {
+        continuePressNumberOrEmail.click();
+        return this;
+    }
+
+    public SignIn setPasswordElement(String password) {
+        passwordElement.val(password);
+        return this;
+    }
+
+    public RabotaBasePage clickPassword() {
+        continuePassword.click();
+        return this;
+    }
 }
